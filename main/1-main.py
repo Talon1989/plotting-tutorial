6a73210c77429d06eb253e0af80754cd86d0b00c
Talon1989 <olivetto1989@mail.com>
2019-05-22 19:42:24 +0200

    .

---
+++ main/1-main.py
@@ -0,0 +1,26 @@
+import pylab as plt
+
+
+def example():
+    mySamples = []
+    myLinear = []
+    myQuadratic = []
+    myCubic = []
+    myExponential = []
+
+    for i in range(0, 30):
+        mySamples.append(i)
+        myLinear.append(i)
+        myQuadratic.append(i**2)
+        myCubic.append(i**3)
+        myExponential.append(1.5**i)
+
+    plt.plot(mySamples, myLinear)
+    plt.plot(mySamples, myQuadratic)
+    plt.plot(mySamples, myCubic)
+    plt.plot(mySamples, myExponential)
+
+    plt.show()
+
+
+example()
